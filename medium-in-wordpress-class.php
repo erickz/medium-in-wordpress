cf558bb27c5b91563377db215de98ecb7d884dea
Erick C. de São Miguel <erickcmiguel@gmail.com>
2019-10-09 13:59:02 -0300

    Added functions to be made

--- medium-in-wordpress-class.php
+++ medium-in-wordpress-class.php
@@ -3,7 +3,7 @@
 class MediumInWp
 {
     public function __construct() {
-
+        $this->syncPostsWithMedium();
     }
 
     /**
@@ -46,5 +46,27 @@ class MediumInWp
         );
 
         update_option(MEDIUM_IN_WP_PREFIX . 'access_token_' . $userId, serialize($dataToken));
+    }
+
+    /**
+     * TODO:
+     * By default Medium doesn't provide the client_id and client_id secret in the user settings page,
+     * so It's necessary to send an email requesting these infos.
+     * @return Boolean
+     */
+    public function sendEmailToMediumRequestingCredentials()
+    {
+
+    }
+
+    /**
+     * TODO:
+     * Enable the sync of the wp`s posts with Medium, so whenever a posts is published in Wordpress
+     * It's published on the medium user`s account
+     * @return Boolean
+     */
+    public function syncPostsWithMedium()
+    {
+
     }
 }
