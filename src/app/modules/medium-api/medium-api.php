e3c1e9fbc052e90bb25098d05d3cf46949c1806a
Erick C. de São Miguel <erickcmiguel@gmail.com>
2019-10-16 17:51:12 -0300

    Removed unecessary files

--- src/app/modules/medium-api/medium-api.php
+++ src/app/modules/medium-api/medium-api.php
@@ -9,7 +9,41 @@ class MediumApi
 
     }
 
-    public function oauth()
+    public function getAccessTokenFromWp($userId = null)
+    {
+        $token = get_option( MEDIUM_IN_WP_PREFIX . 'access_token_' . $userId);
+
+        if (! $token) {
+            return false;
+        }
+
+        $dataToken = unserialize($token);
+
+        if (! isset($dataToken['access_token'])){
+            return false;
+        }
+
+        return $dataToken['access_token'];
+    }
+
+    public function storeAccessTokenToWp($accessToken = '', $expiresIn = null, $userId = null)
+    {
+        $dataToken = array(
+            'access_token' => $accessToken,
+            'expires_in'   => $expiresIn,
+            'user_id'      => $userId
+        );
+
+        update_option(MEDIUM_IN_WP_PREFIX . 'access_token_' . $userId, serialize($dataToken));
+    }
+
+    /**
+     * TODO:
+     * By default Medium doesn't provide the client_id and client_id secret in the user settings page,
+     * so It's necessary to send an email requesting these infos.
+     * @return Boolean
+     */
+    public function sendEmailToMediumRequestingCredentials()
     {
 
     }
---
+++ src/app/modules/sync-posts/sync-posts.php
@@ -0,0 +1,15 @@
+<?php
+
+class SyncPosts
+{
+    /**
+     * TODO:
+     * Enable the sync of the wp`s posts with Medium, so whenever a posts is published in Wordpress
+     * It's published on the medium user`s account
+     * @return Boolean
+     */
+    public function syncPostsWithMedium()
+    {
+
+    }
+}
